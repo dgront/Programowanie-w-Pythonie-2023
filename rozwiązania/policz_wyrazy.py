c71427b669f83a3f7657cca8170f3705a066d775
Dominik Gront <dgront@chem.uw.edu.pl>
2022-02-05 17:48:56 +0100

    Rozwiązanie zadania: suma liczb z pliku

--- rozwiązania/policz_wyrazy.py
+++ rozwiązania/policz_wyrazy.py
@@ -1,19 +1,19 @@
 import operator
 
-slowa = {}                      # pusty słownik na zliczenia wyrazów
-with open("lotr1.txt") as plik: # otwórz plik
-    for linia in plik:          # linijka po linijce
+slowa = {}                              # pusty słownik na zliczenia wyrazów
+with open("lotr1.txt") as plik:         # otwórz plik
+    for linia in plik:                  # linijka po linijce
         wyrazy = linia.strip().split()
-        for w in wyrazy:        # pętla po wyrazach
+        for w in wyrazy:                # pętla po wyrazach
             # tu obcinamy znaki interpunkcyjne z końca wyrazu
-            n = len(w)          # liczba znaków w wyrazie
+            n = len(w)                  # liczba znaków w wyrazie
             if not w[n-1].isalpha():
                 w = w[:n-1]
 # Inny sposób usuwania znaków z końca - z ujemnym indeksowaniem znaków
 #            if not w[-1].isalpha(): w = w[:-2]
-            if w in slowa:      # Aktualizuj licznik
+            if w in slowa:              # Aktualizuj licznik
                 slowa[w] += 1
-            else:               # lub wstaw do slownika
+            else:                       # lub wstaw do slownika
                 slowa[w] = 1
 
 # sortowanie wynikowego słownika
---
+++ rozwiązania/suma_z_pliku.py
@@ -0,0 +1,16 @@
+import random
+
+with open("nazwa.txt","w") as plik:
+    for i in range(10):
+        print(random.random(), file=plik)
+
+suma = 0.0
+mianownik = 0.0
+with open("nazwa.txt") as plik:
+    plik = open("nazwa.txt")
+    for linia in plik:
+        wyrazy = linia.strip().split()
+        suma = suma + float(wyrazy[0])
+        mianownik = mianownik + 1
+
+print(suma/mianownik)
