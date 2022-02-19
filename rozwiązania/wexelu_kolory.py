4683083097ca70ce5067176df712cc2615c13edf
Dominik Gront <dgront@chem.uw.edu.pl>
2022-02-19 17:17:41 +0100

    wexelu_kolory.py koloruje też tekst

--- rozwiązania/wexelu_kolory.py
+++ rozwiązania/wexelu_kolory.py
@@ -1,6 +1,6 @@
 from openpyxl import Workbook
 from openpyxl.styles.colors import Color
-from openpyxl.styles import PatternFill
+from openpyxl.styles import PatternFill, Font
 from colormap import rgb2hex
 
 N = 20
@@ -11,9 +11,12 @@ for row in range(N):
     red = int(255 / N * row)
     for col in range(N):
         blue = int(255 / N * col)
-        color_as_str = rgb2hex(red, 100, blue)
-        arkusz.cell(row+1, col+1, 5)
+        arkusz.cell(row+1, col+1, (row+1) * (col+1))
+        color_tla_as_str = rgb2hex(red, 150, blue)
+        color_txt_as_str = rgb2hex(blue, 10, red)
         komorka = arkusz.cell(row+1, col+1)
-        color = Color(color_as_str[1:], type='rgb')
-        komorka.fill = PatternFill(patternType='solid',fill_type='solid', fgColor=color)
+        color_tla = Color(color_tla_as_str[1:], type='rgb')
+        color_txt = Color(color_txt_as_str[1:], type='rgb')
+        komorka.fill = PatternFill(patternType='solid', fill_type='solid', fgColor=color_tla)
+        komorka.font = Font(color=color_txt_as_str[1:])
 w.save("plik2.xlsx")
