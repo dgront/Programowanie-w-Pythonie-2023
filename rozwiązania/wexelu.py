00b301cc1d95667c3accb03ae1870f010ba8054f
Dominik Gront <dgront@chem.uw.edu.pl>
2022-02-19 10:55:26 +0100

    nowy plik: wexelu.py

---
+++ rozwiązania/wexelu.py
@@ -0,0 +1,12 @@
+import sys
+from math import *
+from random import random as rnd
+from random import randint as rni
+
+from openpyxl import Workbook
+
+w = Workbook()
+arkusz = w.active
+w.save("plik.xlsx")
+
+
