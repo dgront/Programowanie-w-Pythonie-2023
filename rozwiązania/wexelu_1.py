efc6e69f5dd780dfd1bfa99321c60a773946ae6d
Dominik Gront <dgront@chem.uw.edu.pl>
2022-02-19 17:32:46 +0100

    Poprawiony opis zadań

--- rozwiązania/wexelu_1.py
+++ rozwiązania/wexelu_1.py
@@ -1,9 +1,9 @@
 from openpyxl import Workbook
 
-w = Workbook()
-arkusz = w.active
+skoroszyt = Workbook()
+arkusz = skoroszyt.active
 for i in range(10):
     arkusz.cell(1, i + 1, i * i)
-w.save("plik2.xlsx")
+skoroszyt.save("plik2.xlsx")
 
 
