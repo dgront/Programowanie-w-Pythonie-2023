ecdcce52b095f5318e38a6bbb155fef2d52db3ff
Dominik Gront <dgront@chem.uw.edu.pl>
2022-02-19 17:23:23 +0100

    Poprawione zadanie: epitrochoida.py

--- rozwiązania/epitrochoida.py
+++ rozwiązania/epitrochoida.py
@@ -1,24 +1,23 @@
 from math import sin, cos
 from matplotlib import pyplot
 
-N = 140
-krok = 0.1
-t = 0
-R = 100
-r = 15
-h = 10
+N = 1000        # Liczba punktów na wykresie
+krok = 0.1      # jak często punkty?
+R = 100         # promień dużego koła
+r = 15          # promień małego koła
+h = 10          # położenie punktu
 
-for n in range(0,100,10):
-    daneX = []
-    daneY = []
-    for i in range(n):
-        t = t + krok
-        x = (R+r) * cos(t) - h * cos((R+r)/r * t)
-        y = (R+r) * sin(t) - h * sin((R+r)/r * t)
-        daneX.append(x)
-        daneY.append(y)
-    pyplot.axes().set_aspect('equal')
-    pyplot.xlim(-130, 130)
-    pyplot.ylim(-130, 130)
-    pyplot.plot(daneX, daneY)
-    pyplot.show()
+t = 0
+daneX = []      # lista na wyniki - wsp X
+daneY = []      # lista na wyniki - wsp Y
+for i in range(N):
+    t = t + krok
+    x = (R+r) * cos(t) - h * cos((R+r)/r * t)
+    y = (R+r) * sin(t) - h * sin((R+r)/r * t)
+    daneX.append(x)
+    daneY.append(y)
+pyplot.axes().set_aspect('equal')   # niech wykres będzie kwadratowy
+pyplot.xlim(-130, 130)              # zakresy osi x
+pyplot.ylim(-130, 130)              # zakresy osi y
+pyplot.plot(daneX, daneY)           # wykres liniowy
+pyplot.show()                       # pokaż wykres
